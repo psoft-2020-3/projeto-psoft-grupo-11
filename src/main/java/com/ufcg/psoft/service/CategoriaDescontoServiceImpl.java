0615edeb157d263b5c1169d1cdb40d8101f6eb49
Larissa Amorim <larissa.gabriela.costa@ccc.ufcg.edu.br>
2020-11-26 21:30:53 -0300

    Update CategoriaDescontoServiceImpl.java

--- src/main/java/com/ufcg/psoft/service/CategoriaDescontoServiceImpl.java
+++ src/main/java/com/ufcg/psoft/service/CategoriaDescontoServiceImpl.java
@@ -3,10 +3,9 @@ package com.ufcg.psoft.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ufcg.psoft.model.CategoriaDesconto;
+import com.ufcg.psoft.model.Categoria;
 import com.ufcg.psoft.model.TipoDesconto;
 import com.ufcg.psoft.repositories.CategoriaDescontoRepository;
-
 
 @Service
 public class CategoriaDescontoServiceImpl implements CategoriaDescontoService {
@@ -15,14 +14,14 @@ public class CategoriaDescontoServiceImpl implements CategoriaDescontoService {
 	CategoriaDescontoRepository categoriaDescontoRepository;
 
 	@Override
-	public CategoriaDesconto save(CategoriaDesconto categoriaDesconto) {
-		CategoriaDesconto categoriaDescontoSalva = this.categoriaDescontoRepository.save(categoriaDesconto);
+	public Categoria save(Categoria categoriaDesconto) {
+		Categoria categoriaDescontoSalva = this.categoriaDescontoRepository.save(categoriaDesconto);
 		return categoriaDescontoSalva;
 	}
 
 	@Override
-	public CategoriaDesconto update(String categoria, String desconto) throws Exception {
-		CategoriaDesconto categoriaDesconto = this.categoriaDescontoRepository.findByCategoria(categoria.toUpperCase());
+	public Categoria update(String categoria, String desconto) throws Exception {
+		Categoria categoriaDesconto = this.categoriaDescontoRepository.findByCategoria(categoria.toUpperCase());
 		if (categoriaDesconto == null) {
 			throw new Exception("Categoria não existe");
 		}
